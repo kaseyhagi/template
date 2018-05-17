4bfeb58e679b3fec4a4a68e282d1a68b65282b98
kaseyhagi <khagi@hawaii.edu>
2018-05-16 15:38:06 -1000

    resume

---
+++ info.js
@@ -0,0 +1,85 @@
+var info = {
+    intro: "<span style='font-size:25px'>Aloha!</span> <br><br>I'm <span style='font-weight:bold'>Kasey Hagi</span>, a recent graduate and software engineer based in Honolulu. I received my B.S. in Computer Engineering from University of Hawaii at Manoa.",
+    email: "kaseyhagi@gmail.com",
+    phone: "(808)224-7801",
+    linkedin: "",
+    github: "",
+    skills: {
+        languages: [ "Python", "C++", "C", "Java", "Javascript", "HTML/CSS"],
+        tools: ["Github", "MongoDB", "MatLab", "MySQL"],
+        os: ["Windows", "Mac OS", "Linux Ubuntu"]
+    },
+    education: [
+        {
+            school: "University of Hawaii at Manoa",
+            city: "Honolulu, HI",
+            gpa: "3.59/4.0",
+            degree: "B.S. Computer Engineering",
+            grad: "December 2017"
+        }
+    ],
+    projects: [
+        {
+            name: "Feature Recognition in Satellite Imagery using Machine Learning",
+            description: "",
+            when: "Fall 2017",
+            detail: "Using an artificial neural network to detect boats and vessels in satellite images.",
+            tags: ["Python", "Machine Learning", "OpenCV"]
+        },
+        {
+            name: "CosmosPlot",
+            when: "Summer 2017",
+            description: "GUI Data Plotting Tool for COSMOS",
+            detail: "Developed a real-time data plotting tool for data gathered by COSMOS software agents with simplified user customizability and dynamic layout and formatting. Compliant with COSMOS framework and communication protocols. Used by engineers for satellite testbed operations.",
+            tags: ["C++", "Qt Creator"]
+        },
+        {
+            name: "Transpiler Project",
+            when: "Spring 2017",
+            description: "Building a Scheme Interpreter",
+            detail: "Worked closely with a team of student developers and served as Core Systems Team Co-Leader to design and implement the core environment features and evalutation strategies of the Scheme compiler",
+            tags: ["C++"]
+        },
+        {
+            name: "Moar Chow",
+            when: "Fall 2016",
+            description: "Web Application built on Meteor Framework",
+            detail: "Worked closely with a team of student developers to design and implement a web application to provide information on all on-campus food vendors such as location, menu, operating hours.  Incorporated MongoDB to the web application to store vendor information as well as user input reviews and ratings. ",
+            tags: ["JavaScript", "MongoDB", "Semantic UI", "HTML/CSS"]
+        }
+    ],
+    experience: [
+        {
+            company: "Hawaii Space Flight Laboratory",
+            location: "Honolulu, HI",
+            position: "Assistant Software Engineer",
+            description: "Worked directly with a team of engineers and researchers to develop software defined radio for satellite missions. ",
+            startDate: "April 2018",
+            endDate: "Present",
+            tags: ["Python", "C++", "SDR"]
+            
+        },
+        {
+            company: "Hawaii Space Flight Laboratory",
+            location: "Honolulu, HI",
+            position: "Intern - Satellite Testbed Assistant ",
+            description: "Joined a team of engineers and researchers to develop an embedded software interface library for an ADCS unit for a 3U CubeSat platform under the COSMOS framework. ",
+            startDate: "May 2017",
+            endDate: "December 2017",
+            tags: ["COSMOS Project", "C++"]
+            
+        },
+        {
+            company: "City and County of Honolulu",
+            position: "Po'okela Fellows Intern - Department of Information Technology",
+            location: "Honolulu, HI",
+            description: "Worked with developers to produce inter-departmental web applications to facilitate operations. Designed and implemented web forms and surveys linked to MySQL databases. ",
+            startDate: "January 2017",
+            endDate: "June 2017",
+            tags: ["PHP", "Web Development", "MySQL"]
+            
+        }
+        
+    ]
+    
+};
---
+++ resume.js
@@ -0,0 +1,130 @@
+function writeIntro() {
+    var intro = document.getElementById('intro');
+
+    intro.innerHTML += info.intro;
+
+}
+function writeSkills() {
+    var div_lang = document.getElementById('skills_languages');
+    div_lang.innerHTML += '<ul>';
+    for(var i = 0; i < info.skills.languages.length; i++){
+        div_lang.innerHTML += '<li>' + info.skills.languages[i] +'</li>';
+    }
+    div_lang.innerHTML += '</ul>';
+    var div_tools = document.getElementById('skills_tools');
+    div_tools.innerHTML += '<ul>';
+    for(var i = 0; i < info.skills.tools.length; i++){
+        div_tools.innerHTML += '<li>' + info.skills.tools[i] +'</li>';
+    }
+    div_tools.innerHTML += '</ul>';
+    var div_os = document.getElementById('skills_os');
+    div_os.innerHTML += '<ul>';
+    for(var i = 0; i < info.skills.os.length; i++) {
+        div_os.innerHTML += '<li>' + info.skills.os[i] +'</li>';
+    }
+    div_os.innerHTML += '</ul>';
+}
+function createProject(p, div){
+    // project title
+    var pTitle = document.createElement('div');
+//    pTitle.setAttribute('class','project_title');
+    pTitle.innerHTML = '<span class="project-title">'+p.name +'</span>';
+    
+    // description
+    if(p.description != ""){
+        pTitle.innerHTML += ' - <span class="project-desc">'+p.description+'</span>';
+    }
+    // when
+    pTitle.innerHTML += ', <span class="project-date">'+p.when+'</span>';
+
+    div.appendChild(pTitle);
+    var pDetail = document.createElement('div');
+    // detail
+    pDetail.innerHTML+= '<span class="project-detail">'+p.detail+'</span>';
+    div.appendChild(pDetail);        
+        
+    // tags
+    var pTags = document.createElement('div');
+    var tags = [];
+    for(var j = 0; j < p.tags.length; j++){
+        tags[j] = document.createElement('div');
+        tags[j].setAttribute('class','tag');
+        tags[j].innerHTML = p.tags[j];
+        pTags.appendChild(tags[j]);
+    }
+    div.appendChild(pTags);
+    
+}
+function writeProjects(){
+    var projdiv = document.getElementById('projects');
+    var projects = [];
+    for(var i = 0; i < info.projects.length; i++){
+        projects[i] = document.createElement('div');
+        projects[i].setAttribute('class','project')
+        createProject(info.projects[i],projects[i]) 
+        projdiv.appendChild(projects[i]);
+    }
+}
+function createJob(w,div){
+    // job title/ position
+    var jTitle = document.createElement('div');
+    jTitle.innerHTML = '<span class="job-title">'+w.position +'</span>';
+    
+    // company
+
+        jTitle.innerHTML += ' <br> <span class="job-company">'+w.company+'</span>';
+
+    // when
+    jTitle.innerHTML += ', <span class="job-start">'+w.startDate+'</span>';
+    jTitle.innerHTML += ' - <span class="job-end">'+w.endDate+'</span>';
+
+    div.appendChild(jTitle);
+    var jDetail = document.createElement('div');
+    // job detail
+    jDetail.innerHTML+= '<span class="job-desc">'+w.description+'</span>';
+    div.appendChild(jDetail);        
+        
+    // tags
+    var jTags = document.createElement('div');
+    var tags = [];
+    for(var j = 0; j < w.tags.length; j++){
+        tags[j] = document.createElement('div');
+        tags[j].setAttribute('class','tag');
+        tags[j].innerHTML = w.tags[j];
+        jTags.appendChild(tags[j]);
+    }
+    div.appendChild(jTags); 
+}
+function writeJobs(){
+    var workdiv = document.getElementById('experience');
+    var work = [];
+    for(var i = 0; i < info.experience.length; i++){
+        work[i] = document.createElement('div');
+        work[i].setAttribute('class','job')
+        createJob(info.experience[i],work[i]) 
+        workdiv.appendChild(work[i]);
+    }
+}
+function createEdu(e, div) {
+    var eTitle = document.createElement('div');
+    eTitle.innerHTML= '<span class="edu-school">'+e.school +'</span>';
+    eTitle.innerHTML += ' - <span class="edu-city">'+e.city+'</span>';
+    eTitle.innerHTML += ' - <span class="edu-degree">'+e.degree+'</span>';
+    
+    div.appendChild(eTitle);
+    var eDetail = document.createElement('div');
+    eDetail.innerHTML = '<span class="edu-school">'+e.grad +'</span>';
+    eDetail.innerHTML+=  ' - <span class="edu-city">'+e.gpa+'</span>';
+    
+    div.appendChild(eDetail);
+}
+function writeEdu() {
+    var edudiv = document.getElementById('education');
+    var schools = [];
+    for(var i = 0; i < info.education.length; i++){
+        schools[i] = document.createElement('div');
+        schools[i].setAttribute('class','education');
+        createEdu(info.education[i], schools[i]);
+        edudiv.appendChild(schools[i]);
+    }
+}
